e912a7365216ff281a84f83bc917064fb46bb06c
Abbie Cornelius <abbyc@sky.com>
2021-08-30 11:28:07 +0100

    commit tree 367368c2a60b09271fcf36a368d3f7b22bf2e0cf

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static_pages#home"
+  resources :users, only: [:new, :create, :show]
+  resources :attractions
+  get '/new', to: 'users#new'
+  get '/signin', to: 'session#new'
+  post '/signin', to: 'session#create'
+  get '/logout', to: 'session#destroy'
+  post '/rides', to: 'rides#create'
 end
